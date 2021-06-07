a8b208bced1b8013a912d48c749d1cb1f6a98014
asnigurski1 <75775512+asnigurski1@users.noreply.github.com>
2021-06-07 15:06:48 +0300

    Add files via upload

---
+++ harjutus12.java
@@ -0,0 +1,66 @@
+package harjutused;
+
+
+public class harjutus12 {
+	 public static void main(String[] args) {
+		 System.out.println("a)");
+		 System.out.println(" ");
+	 
+	 for (int i = 1; i < 7; i++) {
+		 if (i % 2 != 0) {
+	 for (int j = 0; j < i; j++) {
+	
+		 System.out.print("*");
+	 }
+		 System.out.println("");
+	 }
+	 
+	 
+			 
+	 
+}
+	for (int i = 3; i >= 1; i--) {
+		if (i % 2 != 0) {
+	for (int j = 0; j < i; j++) {
+		System.out.print("*");
+	 }
+		System.out.println("");
+		
+	
+}
+}
+	System.out.println("b)");
+	for(int i = 5; i >= 1; i--) {
+	for (int j = 0; j < i; j++) {
+		System.out.print("*");
+	 }
+		System.out.println("");
+		
+		
+	}
+	System.out.println("c)");
+	for(int i = 1; i < 6; i++) {
+	for (int j = 0; j < i; j++) {
+			System.out.print("*");
+		 }
+			System.out.println("");
+	}
+	System.out.println("d)");
+	for(int i = 5; i >= 1; i--) {
+		for (int j = 0; j < i; j++) {
+			System.out.print("*");
+		 }
+			System.out.println("");
+			
+			
+		}
+		for(int i = 1; i < 6; i++) {
+		for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			 }
+				System.out.println("");
+		}
+	}
+
+}
+
